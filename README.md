a0de0d45806d5166b6abd3837ae32455b310c5fd
Niklas Sombert <niklas@ytvwld.de>
2023-07-21 14:55:51 +0200

    Update documentation

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # update_cell
 
-A Cell<Option<T>> that you can update
+A `Cell<Option<T>>` that you can update
+
+## Why would I need this?
+
+[`Cell::update`] is currently experimental. And it only supports types
+that are `Copy`. So if you want to store and modify something that is
+neither `Copy` nor has a `Default` (eg. a builder), this crate might be
+useful.
 
 ## Usage
 
@@ -9,6 +16,30 @@ Add this to your `Cargo.toml`:
 ```toml
 [dependencies]
 update_cell = "0.1"
+```
+
+And if you have a struct, you can put it inside and modify it:
+
+```rust
+use update_cell::UpdateCell;
+
+struct MySuperFancyStruct {
+    inner: bool
+}
+
+impl MySuperFancyStruct {
+    fn new() -> Self {
+        Self { inner: false }
+    }
+
+    fn toggle(mut self) -> Self {
+        self.inner = !self.inner;
+        self
+    }
+}
+
+let mut cell = UpdateCell::new(MySuperFancyStruct::new());
+cell.update(|s| s.toggle());
 ```
 
 License: MPL-2.0
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,11 @@
-//! A Cell<Option<T>> that you can update
+//! A `Cell<Option<T>>` that you can update
+//! 
+//! # Why would I need this?
+//! 
+//! [`Cell::update`] is currently experimental. And it only supports types
+//! that are `Copy`. So if you want to store and modify something that is
+//! neither `Copy` nor has a `Default` (eg. a builder), this crate might be
+//! useful.
 //!
 //! # Usage
 //!
@@ -8,25 +15,55 @@
 //! [dependencies]
 //! update_cell = "0.1"
 //! ```
+//! 
+//! And if you have a struct, you can put it inside and modify it:
+//! 
+//! ```rust
+//! use update_cell::UpdateCell;
+//! 
+//! struct MySuperFancyStruct {
+//!     inner: bool
+//! }
+//!
+//! impl MySuperFancyStruct {
+//!     fn new() -> Self {
+//!         Self { inner: false }
+//!     }
+//!
+//!     fn toggle(mut self) -> Self {
+//!         self.inner = !self.inner;
+//!         self
+//!     }
+//! }
+//! 
+//! let mut cell = UpdateCell::new(MySuperFancyStruct::new());
+//! cell.update(|s| s.toggle());
+//! ```
 
 #![no_std]
 #![doc(html_root_url = "https://docs.rs/update_cell/0.1.0")]
 use core::cell::Cell;
 
+/// The main struct.
+/// 
+/// Put whatever you want in here.
 pub struct UpdateCell<T> {
     value: Cell<Option<T>>
 }
 
 impl<T> UpdateCell<T> {
+    /// Create a new instance.
     pub fn new(val: T) -> Self {
         Self { value: Cell::new(Some(val)) }
     }
     
+    /// Modify the contained value by passing a function.
     pub fn update<F: FnOnce(T) -> T>(&mut self, func: F) {
         let val = self.value.take().unwrap();
         self.value.set(Some(func(val)))
     }
 
+    /// Resolve the cell into the inner value.
     pub fn into_inner(self) -> T {
         self.value.take().unwrap()
     }
